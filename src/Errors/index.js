c7e200a6014dbf34847af66397333f292922cb6f
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-03 11:37:28 +0200

    Error initialization

--- src/Errors/index.js
+++ src/Errors/index.js
@@ -2,13 +2,13 @@
 import IllegalTypeInForAny from './IllegalTypeInForAny.js';
 import IllegalUsageOfArgumentInIsArrayAndForEvery from './IllegalUsageOfArgumentInIsArrayAndForEvery.js';
 import IncorrectArgumentInBindMethod from './IncorrectArgumentInBindMethod.js';
-import IncorrectArgumentInIsEmpty from './IncorrectArgumentInIsEmpty';
+import IncorrectArgumentInIsEmpty from './IncorrectArgumentInIsEmpty.js';
 import IncorrectArgumentInStringContainsExpression from './IncorrectArgumentInStringContainsExpression.js';
 import IncorrectArgumentInHasLength from './IncorrectArgumentInHasLength.js';
 import IncorrectArgumentsInInterface2 from './IncorrectArgumentsInInterface2.js';
 import IncorrectFunctionArgumentInForAll from './IncorrectFunctionArgumentInForAll.js';
 import IncorrectFunctionArgumentInForAny from './IncorrectFunctionArgumentInForAny.js';
-import IncorrectTypeInStringContains from './IncorrectTypeInStringContains'
+import IncorrectTypeInStringContains from './IncorrectTypeInStringContains.js'
 export {
     IllegalTypeInForAny,
     IllegalUsageOfArgumentInIsArrayAndForEvery,
