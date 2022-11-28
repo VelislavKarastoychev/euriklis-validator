f7afb16e174282aa33a6a26cb968ad968b08015c
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 13:49:22 +0200

    -m

---
+++ src/Warnings/IncorrectValueInIsNegative.js
@@ -0,0 +1,7 @@
+'use strict';
+import * as texts from './texts.js';
+const IncorrectValueInIsNegative = () => {
+    console.log(texts.WarningMessage);
+    console.log(texts.IncorrectValueInIsNegative);
+}
+export default IncorrectValueInIsNegative;
