375f1230d120fcfda7dd22cb33145ea5d650ddd5
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 14:02:50 +0200

    warning message for not function in the on() method.

--- src/Warnings/IncorrectFunctionInOnMethod.js
+++ src/Warnings/IncorrectFunctionInOnMethod.js
@@ -1,10 +1,7 @@
 'use strict';
-import message from '@euriklis/message';
-import { Printer as printer } from '../Printer/index.js';
 import * as texts from './texts.js';
 const warning = () => {
-    return new message().append(printer.HeaderMessage(texts.WarningMessage))
-        .append(printer.ErrorMessage(texts.IncorrectFunctionInOnMethod))
-        .reset().log();
+    console.log(texts.WarningMessage)
+    console.log(texts.IncorrectFunctionInOnMethod)
 };
 export default warning;
