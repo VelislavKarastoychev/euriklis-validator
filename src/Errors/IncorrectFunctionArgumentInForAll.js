8e71867e9e493fa1e4fcfb0a6d46a43c7bb940d2
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 13:59:35 +0200

    -m

--- src/Errors/IncorrectFunctionArgumentInForAll.js
+++ src/Errors/IncorrectFunctionArgumentInForAll.js
@@ -1,10 +1,9 @@
 'use strict';
-import {Printer as printer} from '../Printer/index.js';
 import * as texts from './texts.js'; 
 const error = () => {
     const error = new Error();
-    error.name = printer.HeaderMessage(texts.ErrorMessage);
-    error.message = printer.ErrorMessage(texts.IncorrectFunctionArgumentInForAll);
+    error.name = texts.ErrorMessage;
+    error.message = texts.IncorrectFunctionArgumentInForAll;
     throw error;
 };
 export default error;
