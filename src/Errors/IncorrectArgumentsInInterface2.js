a1e0c39ec9b0c56cac874e56c6f21587dee7dff1
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 13:58:41 +0200

    -m

--- src/Errors/IncorrectArgumentsInInterface2.js
+++ src/Errors/IncorrectArgumentsInInterface2.js
@@ -1,10 +1,9 @@
 'use strict';
-import {Printer as printer} from '../Printer/index.js';
 import * as texts from './texts.js';
 const error = () => {
     const error = new Error();
-    error.name = printer.HeaderMessage(texts.ErrorMessage);
-    error.message = printer.ErrorMessage(texts.IncorrectArgumentInInterface2);
+    error.name = texts.ErrorMessage;
+    error.message = texts.IncorrectArgumentInInterface2;
     throw error;
 };
 export default error;
