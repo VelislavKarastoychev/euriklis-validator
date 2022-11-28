60d67d5df8c71346ef869a292084ccaf279ccb43
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 13:50:21 +0200

    -m

---
+++ src/Warnings/IncorrectValueInIsPositive.js
@@ -0,0 +1,7 @@
+'use strict';
+import * as texts from './texts.js';
+const IncorrectValueInIsPositive = () => {
+    console.log(texts.WarningMessage);
+    console.log(texts.IncorrectValueInIsPositive)
+}
+export default IncorrectValueInIsPositive;
