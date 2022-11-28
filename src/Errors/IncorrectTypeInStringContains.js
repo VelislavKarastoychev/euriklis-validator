33e8ceb804902a51702173f037454fdc556150c3
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 14:01:07 +0200

    error message for string contains method

--- src/Errors/IncorrectTypeInStringContains.js
+++ src/Errors/IncorrectTypeInStringContains.js
@@ -1,9 +1,8 @@
 'use strict';
-import { Printer as printer } from "../Printer/index.js";
 import * as texts from './texts.js';
 const error = () => {
     const error = new Error();
-    error.name = printer.HeaderMessage(texts.ErrorMessage);
-    error.message = printer.ErrorMessage(texts.IncorrectTypeInStringContains);
+    error.name = texts.ErrorMessage;
+    error.message = texts.IncorrectTypeInStringContains;
 };
 export default error;
