ba1f27b1dd3edc041783a140bbc52a4d338c6827
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 13:53:57 +0200

    -m

--- src/Errors/IllegalUsageOfArgumentInIsArrayAndForEvery.js
+++ src/Errors/IllegalUsageOfArgumentInIsArrayAndForEvery.js
@@ -1,10 +1,9 @@
 'use strict';
-import {Printer as printer} from '../Printer/index.js';
 import * as texts from './texts.js';
 const error = () => {
     const error = new Error();
-    error.name = printer.HeaderMessage(texts.ErrorMessage);
-    error.message = printer.ErrorMessage(texts.IllegalUsageOfArgumentInIsArrayAndForEvery);
+    error.name = texts.ErrorMessage;
+    error.message = texts.IllegalUsageOfArgumentInIsArrayAndForEvery;
     throw error;
 };
 export default error; 
