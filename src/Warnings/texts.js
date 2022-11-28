0f96ebbf48caa4bde804ad094c02176ab0d2b486
VelislavKarastoychev <exel_mmm@abv.bg>
2022-11-28 14:04:43 +0200

    Texts for all the warning messages.

--- src/Warnings/texts.js
+++ src/Warnings/texts.js
@@ -1,9 +1,13 @@
 'use strict';
 const IncorrectFunctionInOnMethod = 'Incorrect function argument in on method of the validator method.';
 const IncorrectStateInOnMethod = 'Incorrect state argument in the on method in the validator method.';
+const IncorrectValueInIsNegative = 'Incorrect value property of the current validator instance. Note that if the value is not number, the is_negative() / isNegative method is not usable. The method returns false, but the result may be incorrect.';
+const IncorrectValueInIsPositive = 'Incorrect value property of the current validator instance. Note that if the value is not number, the is_positive() / isPositive method is not usable. The method returns false, but the result may be incorrect.';
 const WarningMessage = 'Euriklis validator library warning message:';
 export {
     IncorrectFunctionInOnMethod,
     IncorrectStateInOnMethod,
+    IncorrectValueInIsNegative,
+    IncorrectValueInIsPositive,
     WarningMessage,
 };
