9d95b4c00300f9c2365fc6dfe4e931d6f9d5a61e
VelislavKarastoychev <exel_mmm@abv.bg>
2023-01-18 23:05:38 +0200

    Implementation of the @euriklis/validator library.

--- src/validator.js
+++ src/validator.js
@@ -1873,6 +1873,59 @@ class validator {
         } else this.#question = false;
         return this.#set_answer();
     }
+    is_array_of_number_arrays_with_equal_size () { return this.isArrayOfNumberArraysWithEqualSize; }
+    get isArrayOfNumberArraysWithEqualSize () {
+        this.#question = true;
+        if (this.copy().isArray.answer) {
+            const is_first_item_array = new validator(this.value[0]).isArray.answer;
+            const n = this.value.length;
+            if (is_first_item_array) {
+                const m = this.value[0].length;
+                let i, j;
+                for (i = 0;i < n >> 2;i++) {
+                    j = i << 2;
+                    if (!new validator(this.value[j]).isNumberArray.And.has_length(m).answer) {
+                        this.#question = false;
+                        break;
+                    }
+                    ++j;
+                    if (!new validator(this.value[j]).isNumberArray.And.has_length(m).answer) {
+                        this.#question = false;
+                        break;
+                    }
+                    ++j;
+                    if (!new validator(this.value[j]).isNumberArray.And.has_length(m).answer) {
+                        this.#question = false;
+                        break;
+                    }
+                    ++j;
+                    if (!new validator(this.value[j]).isNumberArray.And.has_length(m).answer) {
+                        this.#question = false;
+                        break;
+                    }
+                }
+                if (this.#question && (n % 4 >= 3)) {
+                    j = n - 3;
+                    if (!new validator(this.value[j]).isNumberArray.And.has_length(m).answer) {
+                        this.#question = false;
+                    }
+                }
+                if (this.#question && (n % 4 >= 2)) {
+                    j = n - 2;
+                    if (!new validator(this.value[j]).isNumberArray.And.has_length(m).answer) {
+                        this.#question = false;
+                    }
+                }
+                if (this.#question && (n % 4 >= 1)) {
+                    j = n - 1;
+                    if (!new validator(this.value[j]).isNumberArray.And.has_length(m).answer) {
+                        this.#question = false;
+                    }
+                }
+            } else this.#question = false;
+        } else this.#question = false;
+        return this.#set_answer();
+    }
     is_array_of_string_arrays() { return this.isArrayOfStringArrays; }
     get isArrayOfStringArrays() {
         this.#question = true;
