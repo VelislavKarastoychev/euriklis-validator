7c0e5d5071c432c97aac46dffe14d621d11dc1b7
VelislavKarastoychev <exel_mmm@abv.bg>
2023-07-28 11:45:40 +0300

    added isArray and isPositiveIntegerLike.

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,4 +1,5 @@
 "use strict";
+export { IsArray } from "./IsArray.js";
 export { IsBoolean } from "./IsBoolean.js";
 export { IsInteger } from "./IsInteger.js";
 export { IsIntegerLike } from "./IsIntegerLike.js";
@@ -6,6 +7,7 @@ export { IsNegativeIntegerLike } from "./IsNegativeIntegerLike.js";
 export { IsNegativeNumberLike } from "./IsNegativeNumberLike.js";
 export { IsNumber } from "./IsNumber.js";
 export { IsNumberLike } from "./IsNumberLike.js";
+export { IsPositiveIntegerLike } from "./IsPositiveIntegerLike.js";
 export { IsPositiveNumberLike } from "./IsPositiveNumberLike.js";
 export { IsString } from "./IsString.js";
 export { IsUndefined } from "./IsUndefined.js";
