2256c4f4eb6ee7bb89446a4f60382f96ba2ac762
VelislavKarastoychev <exel_mmm@abv.bg>
2023-07-28 09:25:28 +0300

    implementation of the isInteger method.

---
+++ src/Models/IsInteger.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * Tests if the value parameter is integer.
+ * Utility 
+ * @param {any} value
+ * @returns {boolean}
+ **/
+export const IsInteger = (value) => (typeof value === 'number') && (Number.isInteger(value));
