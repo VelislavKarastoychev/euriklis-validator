7a9eebfbd30591af9014e67f0ce788ad26230f14
VelislavKarastoychev <exel_mmm@abv.bg>
2023-07-28 09:25:53 +0300

    add isInteger and isNumber methods.

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,4 +1,6 @@
 "use strict";
 export { IsBoolean } from "./IsBoolean.js";
+export { IsInteger } from "./IsInteger.js";
+export { IsNumber } from "./IsNumber.js";
 export { IsString } from "./IsString.js";
 export { IsUndefined } from "./IsUndefined.js";
