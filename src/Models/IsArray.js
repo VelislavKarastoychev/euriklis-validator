da847fbfd2da0a7a90bd5a5ea53896e97aa4ad22
VelislavKarastoychev <exel_mmm@abv.bg>
2023-07-28 11:44:42 +0300

    implementation of the is array method.

---
+++ src/Models/IsArray.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * Tests if the value parameter is Array type.
+ * Utility function for the isArray method.
+ * @param {Array} value
+ * @returns {boolean}
+ */
+export const IsArray = (value) => value instanceof Array;
