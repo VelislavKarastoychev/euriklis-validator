8c88dbaf80ff89006c8895d517b5f3734548c5b5
VelislavKarastoychev <exel_mmm@abv.bg>
2023-07-28 09:56:41 +0300

    added isNumberLike and isIntegerLike.

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,6 +1,8 @@
 "use strict";
 export { IsBoolean } from "./IsBoolean.js";
 export { IsInteger } from "./IsInteger.js";
+export { IsIntegerLike } from "./IsIntegerLike.js";
 export { IsNumber } from "./IsNumber.js";
+export { IsNumberLike } from "./IsNumberLike.js";
 export { IsString } from "./IsString.js";
 export { IsUndefined } from "./IsUndefined.js";
