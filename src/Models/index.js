49bd3681c0390793f437d8bbc24a47055a4a32cd
VelislavKarastoychev <exel_mmm@abv.bg>
2023-07-28 10:05:38 +0300

    added isNegativeNumberLike

--- src/Models/index.js
+++ src/Models/index.js
@@ -2,6 +2,7 @@
 export { IsBoolean } from "./IsBoolean.js";
 export { IsInteger } from "./IsInteger.js";
 export { IsIntegerLike } from "./IsIntegerLike.js";
+export { IsNegativeNumberLike } from "./IsNegativeNumberLike.js";
 export { IsNumber } from "./IsNumber.js";
 export { IsNumberLike } from "./IsNumberLike.js";
 export { IsString } from "./IsString.js";
