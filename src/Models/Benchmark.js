e1eddc142ee0d1dfa4f97508fc346feb67e27661
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-01 06:16:44 +0300

    implementation of the benchmark method.This method is new!

---
+++ src/Models/Benchmark.js
@@ -0,0 +1,36 @@
+"use strict";
+/**
+ * @callback {benchmarkCallback}
+ * @param {any} parameters
+ * @description a benchmark function which will be executed
+ * in order to be computed their performance.
+ **/
+/**
+ * @param {any} params - the current validator 
+ * value which will be passed as argument in the
+ * benchmark callback function.
+ * @param {benchmarkCallback} f - a function which will be
+ * executed "iteration" times.
+ * @param {number} iterations - a positive integer which represents
+ * the number of the iterations.
+ * @returns {mean: number, std: number} an object with properties mean and
+ * std, which has the values of the average 
+ **/
+export const Benchmark = (params, f, iterations) => {
+ let dt1, dt2, times = [];
+  for (let i = iterations;i--;) {
+    dt1 = performance.now();
+    f(params);
+    dt2 = performance.now();
+    times.push(dt2 - dt1);
+  }
+  // compute the mean (average) time performance:
+  const mean = times.reduce((m, value) => m + value, 0) / iterations;
+  // compute the standard deviation:
+  const dispersion = times.reduce((sigma, value) => {
+    const diff = value - mean;
+    return sigma + diff * diff;
+  }, 0);
+  const std = Math.sqrt(dispersion / (iterations - 1));
+  return { mean, std };
+}
