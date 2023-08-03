167ffe28cb6d63a6e9c86b780ef41487345dcdbd
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-03 15:51:45 +0300

    Implementation of the isStringArray.

--- src/Models/IsStringArray.js
+++ src/Models/IsStringArray.js
@@ -3,51 +3,53 @@
  * Utility function for the isStringArray method.
  * This funciton sets the value of the private question
  * property of the current validator instance.
- * 
+ *
  * @param {string []} value - the current validator value
  * property, which has to be a string array.
- * @param {boolean} question - the private property question of the
+ * @returns {boolean} question or the private property question of the
  * current validator instance.
- * 
+ *
  })
  **/
-export const IsStringArray = (value, question) => {
+export const IsStringArray = (value) => {
   let i, j, n = value.length;
-      for (i = 0; i < n; i++) {
-        j = i << 2;
-        if (typeof value[j] !== "string") {
-          question = false;
-          break;
-        } else question = true;
-        ++j;
-        if (typeof value[j] !== "string") {
-          question = false;
-          break;
-        } else question = true;
-        ++j;
-        if (typeof value[j] !== "string") {
-          question = false;
-          break;
-        } else question = true;
-        ++j;
-        if (typeof value[j] !== "string") {
-          question = false;
-          break;
-        } else question = true;
-      }
-      if (question && n % 4 >= 1) {
-        j = n - 1;
-        if (typeof value[j] !== "string") question = false;
-        else question = true;
-      }
-      if (question && n % 4 >= 2) {
-        j = n - 2;
-        if (typeof value[j] !== "string") question = false;
-        else question = true;
-      }
-      if (question && n % 4 >= 3) {
-        j = n - 3;
-        if (typeof this.value[j] !== "string") question = false;
-        else question = true;
-      }
-}
+  let question = true;
+  for (i = 0; i < n >> 2; i++) {
+    j = i << 2;
+    if (typeof value[j] !== "string") {
+      question = false;
+      break;
+    } else question = true;
+    ++j;
+    if (typeof value[j] !== "string") {
+      question = false;
+      break;
+    } else question = true;
+    ++j;
+    if (typeof value[j] !== "string") {
+      question = false;
+      break;
+    } else question = true;
+    ++j;
+    if (typeof value[j] !== "string") {
+      question = false;
+      break;
+    } else question = true;
+  }
+  if (question && n % 4 >= 1) {
+    j = n - 1;
+    if (typeof value[j] !== "string") question = false;
+    else question = true;
+  }
+  if (question && n % 4 >= 2) {
+    j = n - 2;
+    if (typeof value[j] !== "string") question = false;
+    else question = true;
+  }
+  if (question && n % 4 >= 3) {
+    j = n - 3;
+    if (typeof value[j] !== "string") question = false;
+    else question = true;
+  }
+  return question;
+};
