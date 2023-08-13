655bb9350396889c7ee59befe91522781230b482
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 16:24:25 +0300

    deleted

--- src/Errors/IllegalParametersInIsArrayOfNumbersInRange.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const IllegalParametersInIsArrayOfNumbersInRange = () => {
-    const error = new Error();
-    error.name = 'euriklis/validator error message:';
-    error.message = 'Illegal parameters in the is_array_of_numbers_in_range() method. Note that these parameters have to be numbers and in addition the first parameter has to be less than the second.';
-    throw error;
-}
-export default IllegalParametersInIsArrayOfNumbersInRange;
