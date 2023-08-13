e0fbf7eedc02241d9e99bd376528d5cdc998158a
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 16:42:33 +0300

    deleted

--- src/Errors/IncorrectArgumentInHasLengthBiggerThan.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const IncorrectArgumentInHasLengthBiggerThan = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentInHasLengthBiggerThan;
-    throw error;
-}
-export default IncorrectArgumentInHasLengthBiggerThan;
