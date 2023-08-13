c2a6e1490f59e8d5fd6f8f5369af0b3d2b44a7ae
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 16:28:20 +0300

    deleted

--- src/Errors/IllegalTypeInForAny.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const error = (value) => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IllegalTypeInForAny(value);
-    throw error;
-};
-export default error;
