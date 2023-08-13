917eb45bb1d44f16236de8c870c01e527a786512
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 19:49:58 +0300

    deleted

--- src/Errors/IncorrectArgumentsInInterface2.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const error = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentInInterface2;
-    throw error;
-};
-export default error;
