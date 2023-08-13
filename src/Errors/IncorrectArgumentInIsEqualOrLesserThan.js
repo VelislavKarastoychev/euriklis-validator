2933033df8e70159ea1fcdb06bdc7dbd840afdba
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 19:11:37 +0300

    deleted

--- src/Errors/IncorrectArgumentInIsEqualOrLesserThan.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const IncorrectArgumentInIsEqualOrLesserThan = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentInIsEqualOrLesserThan;
-    throw error;
-}
-export default IncorrectArgumentInIsEqualOrLesserThan;
