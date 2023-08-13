ab32ef4d66355ea7e8799a5ba7e30d013b975006
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 20:08:45 +0300

    deleted

--- src/Errors/IncorrectTypeInStringContains.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const error = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectTypeInStringContains;
-};
-export default error;
