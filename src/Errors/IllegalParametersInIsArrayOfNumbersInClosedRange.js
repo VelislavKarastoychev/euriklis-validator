0685ee097a77eb129e142b6d879e045615c2f510
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 16:20:56 +0300

    deleted

--- src/Errors/IllegalParametersInIsArrayOfNumbersInClosedRange.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const IllegalParametersInIsArrayOfNumbersInClosedRange = () => {
-    const error = new Error();
-    error.name = '@euriklis/validator error message:';
-    error.message = 'Illegal parameters in is_array_of_numbers_in_closed_range(a, b) method. Note that the parameters of the method have to be numbers and in addition the first has to be smaller than the second.';
-    throw error;
-}
-export default IllegalParametersInIsArrayOfNumbersInClosedRange;
