4b23e169e2426838e7dda5348efe2d2d42d6e079
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 18:54:26 +0300

    deleted

--- src/Errors/IncorrectArgumentInHasLengthLesserThan.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js'; 
-const IncorrectArgumentInHasLengthLesserThan = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentInHasLengthLesserThan;
-    throw error;
-}
-export default IncorrectArgumentInHasLengthLesserThan; 
