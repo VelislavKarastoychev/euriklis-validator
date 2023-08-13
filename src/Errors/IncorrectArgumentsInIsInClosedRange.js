96b855660034b1a0db8c94261b78833655f10016
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 19:53:21 +0300

    deleted

--- src/Errors/IncorrectArgumentsInIsInClosedRange.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-import * as texts from './texts.js';
-const IncorrectArgumentsInIsInClosedRange = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentsInIsInClosedRange;
-    throw error;
-}
-export default IncorrectArgumentsInIsInClosedRange;
