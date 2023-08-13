93df5e8146f48b0b27fca4197d9217dfa3b9fffe
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 18:59:51 +0300

    deleted

--- src/Errors/IncorrectArgumentInIsArrayAndForAny.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const error = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentInIsArrayAndForAny;
-    throw error;
-};
-export default error;
