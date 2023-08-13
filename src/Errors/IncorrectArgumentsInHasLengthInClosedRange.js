835d0a95ff8e95f35d7a2d683f1cf893f0e6c5db
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 19:44:00 +0300

    deleted

--- src/Errors/IncorrectArgumentsInHasLengthInClosedRange.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const IncorrectArgumentsInHasLengthInClosedRange = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentsInHasLengthInClosedRange;
-    throw error;
-}
-export default IncorrectArgumentsInHasLengthInClosedRange;
