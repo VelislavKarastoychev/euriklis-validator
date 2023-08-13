0f57bb7a6c1bc40a5477443b494272e7331057b4
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-13 19:56:09 +0300

    deleted

--- src/Errors/IncorrectArgumentsInIsInRange.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const IncorrectArgumentsInIsInRange = () => {
-    const error = new Error();
-    error.name = texts.ErrorMessage;
-    error.message = texts.IncorrectArgumentsInIsInRange;
-    throw error;
-}
-export default IncorrectArgumentsInIsInRange;
