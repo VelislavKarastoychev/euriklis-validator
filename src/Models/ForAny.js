48d251a0bd46a38f30036ca16fa68f0d96e37ba4
VelislavKarastoychev <exel_mmm@abv.bg>
2023-08-26 17:25:00 +0300

    Implemented for any methods for object and arrays.

--- src/Models/ForAny.js
+++ src/Models/ForAny.js
@@ -1,34 +1,81 @@
 "use strict";
-// 10011011100011101
-// let i = 10;
-// i << 1
-// 
+import validator from "../validator.js";
 export const ForAnyArrayEdition = (value, callback) => {
   let i, j, question = false;
   const n = value.length;
   for (i = 0;i < n >> 2;i++) {
     j = i << 2;
+    if (callback(value[j],j)) {
+      question = true;
+      break;
+    }
     ++j;
+    if (callback(value[j], j)) {
+      question = true;
+      break;
+    }
     ++j;
+    if (callback(value[j], j)) {
+      question = true;
+      break;
+    }
     ++j;
+    if (callback(value[j], j)) {
+      question = true;
+      break;
+    }
   }
   if (n % 4 >= 3 && question) {
     j = n - 3;
+    question = callback(new validator(value[j]), j);
   }
   if (n % 4 >= 2 && question) {
     j = n - 2;
+    question = callback(new validator(value[j]), j);
   }
-  // write the rest of the code here
+  if (n % 4 >= 1 && question) {
+    j = n - 1;
+    question = callback(new validator(value[j]), j);
+  }
   return question;
 }
 export const ForAnyObjectEdition = (value, callback) => {
   let i, j, question = false;
-  const n = value.length;
+  const keys = Object.keys(value);
+  const n = keys.length;
   for (i = 0;i < n >> 2;i++) {
     j = i << 2;
+    if (callback(new validator(value[keys[j]]), j)) {
+      question = true;
+      break;
+    }
     ++j;
+    if (callback(new validator(value[keys[j]]), j)) {
+      question = true;
+      break;
+    }
     ++j;
+    if (callback(new validator(value[keys[j]]), j)) {
+      question = true;
+      break;
+    }
     ++j;
+    if (callback(new validator(value[keys[j]]),j )) {
+      question = true;
+      break;
+    }
+  }
+  if (n % 4 >= 3 && question) {
+    j = n - 3;
+    question = callback(new validator(value[keys[j]]), j);
+  }
+  if (n % 4 >= 2 && question) {
+    j = n - 2;
+    question = callback(new validator(value[keys[j]]), j);
+  }
+  if (n % 4 >= 1 && question) {
+    j = n - 1;
+    question = callback(new validator(value[keys[j]]), j);
   }
   return question;
 }
