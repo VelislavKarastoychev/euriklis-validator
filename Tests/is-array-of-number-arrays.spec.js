22e21956f309b57f5141b9401420e38d0cee9fa2
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-02 21:15:52 +0300

    added print import

--- Tests/is-array-of-number-arrays.spec.js
+++ Tests/is-array-of-number-arrays.spec.js
@@ -1,6 +1,6 @@
 "use strict";
 import validator from "../index.js";
-import { matrix } from "./data.js";
+import { matrix, print } from "./data.js";
 new validator(matrix).isArrayOfNumberArrays.on(true, (v) => {
   const t1 = v.benchmark((n) => new validator(n).isArrayOfNumberArrays.answer);
   const t2 = v.benchmark((n) =>
