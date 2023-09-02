4b008f32df98bf700977f032087731e426c2d27e
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-02 17:48:52 +0300

    tests for the isObjectArray added a test

--- Tests/is-object-array.js
+++ Tests/is-object-array.js
@@ -10,4 +10,19 @@ new validator(objectArray).isObjectArray.on(true, (v) => {
   );
   print("isObjectArray with 1000000 elements", t1, t2);
 });
+objectArray.push(() => Math.random());
+new validator(objectArray).isObjectArray.on(false, (v) => {
+  const t1 = v.benchmark((n) => new validator(n).isObjectArray.answer);
+  const t2 = v.benchmark((n) =>
+    n instanceof Array
+      ? n.every((el) => el instanceof Object && !(el instanceof Array))
+      : false
+  );
+  print(`isObjectArray with ${v.value.length} elements`, t1, t2);
+  objArr.pop(); // finally the objArr will remain an objArr.
+}).on(
+  true,
+  () => console.log("Something went wrong with isObjectArray method!"),
+);
+
 
