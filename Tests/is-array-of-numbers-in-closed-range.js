8113c9ced5728486d519193b275b458cc1a38f29
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-02 16:53:07 +0300

    tests for the method isArrayOfNumbersInClosedRange.

---
+++ Tests/is-array-of-numbers-in-closed-range.js
@@ -0,0 +1,26 @@
+"use strict";
+import validator from "../index.js";
+import { numericArray } from "./data.js";
+new validator(numericArray.is_array_of_numbers_in_closed_range(0, 100).on(
+  true,
+  (v) => {
+    const t1 = v.benchmark((n) =>
+      new validator(n).is_array_of_numbers_in_closed_range(0, 100).answer
+    );
+    const t2 = v.benchmark((n) =>
+      n instanceof Array
+        ? n.every((item) =>
+          (typeof item === "number" && !isNaN(item))
+            ? item <= 100 && item >= 0
+            : false
+        )
+        : false
+    );
+    print(
+      "is_array_of_numbers_in_closed_range(0, 100) with 1000000 elements",
+      t1,
+      t2,
+    );
+  },
+).on(false, () => console.log("Something went wrong ..."));
+
