53581ad4fd1dd038d3db1af5d5055b51dc708b3b
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-06 20:18:10 +0300

    Implementation of the IsInt8Array.js

---
+++ src/Models/IsInt8Array.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * Implements the isInt8Array method. 
+ * @param {Int8Array} value - the current validator "value" instance.
+ * @returns {boolean} if the "value" is Int8Array then returns true, otherwise returns false.
+ **/
+export const IsInt8Array = (value) => {
+  return value instanceof Int8Array;
+} 
