4b662e39ef9d5c21084c5e9769563c7dbac4aac7
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-06 21:49:27 +0300

    implements the isInt32Array

---
+++ src/Models/IsInt32Array.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Utility funciton which is used in the implementation of the isInt32Array.
+ * @param {Int32Array} value - the "value" property of the the current validator instance.
+ * @returns {boolean} if the "value" is Int32Array instance returns true, otherwise returns false.
+ **/
+export const IsInt32Array = (value) => value instanceof Int32Array;
