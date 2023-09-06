fb339bf39167df531bbb3e739ecc6422275f01a3
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-06 13:09:43 +0300

    tests runner.

--- bin/validator.sh
+++ bin/validator.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Determine the path to the script, accounting for global vs local usage
@@ -12,12 +11,12 @@ fi
 
 # Go to the directory containing the script
 cd "$(dirname "$script_path")"
-
+echo -e "$(pwd)"
 # Check if the "Tests" folder exists, otherwise try "tests"
-if [ -d "Tests" ]; then
-    tests_folder="Tests"
-elif [ -d "tests" ]; then
-    tests_folder="tests"
+if [ -d "../Tests" ]; then
+    tests_folder="../Tests"
+elif [ -d "../tests" ]; then
+    tests_folder="../tests"
 else
     echo "No 'Tests' or 'tests' folder found."
     exit 1
@@ -26,9 +25,9 @@ fi
 # Find all test files ending with "spec.js" and run them using Node.js
 for file in "$tests_folder"/*spec.js; do
     if [ -f "$file" ]; then
-        echo "Running $file"
+        # echo "Running $file"
         node "$file"
-        echo "Finished running $file"
+        # echo "Finished running $file"
         echo
     fi
 done
