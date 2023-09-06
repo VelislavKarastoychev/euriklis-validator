c58657c73953faf61b3f1b0fbde2b4da1702e409
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-06 23:45:09 +0300

    implemented utility funciton for the isUint32Array

---
+++ src/Models/IsUint32Array.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Utility function which is used in the implementation of the isUint32Array.
+ * @param {Uint32Array} value - the current validator "value" property.
+ * @returns {boolean} if the "value" property is Uint32Array, then returns true, otherwise returns false.
+ **/
+export const IsUint32Array = (value) => value instanceof Uint32Array; 
