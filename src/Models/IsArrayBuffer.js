9f29493e86017ad29dbf1649006a4d4b3053400b
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-08 23:09:15 +0300

    Implementation of the IsArrayBuffer.

---
+++ src/Models/IsArrayBuffer.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Utility function which is used in the isArrayBuffer method.
+ * @param {ArrayBuffer} value - the current validator "value" property.
+ * @returns {boolean} if the "value" is ArrayBuffer, returns true, otherwise returns false.
+ **/
+export const IsArrayBuffer = (value) => value instanceof ArrayBuffer;
