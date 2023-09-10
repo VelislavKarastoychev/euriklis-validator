519ecc7ef698f03880d07f604672df01a162aa6d
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-10 09:00:09 +0300

    Abstract implementation for the isInstanceOf function name CheckType

---
+++ src/Models/CheckType.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * Utility function which is used in series of methods to check if a variable is of a given type or instance.
+ * @param {Object} value - the current validator "value" property.
+ * @returns {boolean} if the "value" is instance of the "type", then returns true, otherwise returns false.
+ **/
+export const CheckType = (value, type) => Object.prototype.toString.call(value) === `[object ${type}]`;
