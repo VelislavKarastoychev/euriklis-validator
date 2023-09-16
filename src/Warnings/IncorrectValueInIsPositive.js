0e05a560ce2097f72435a8515edc20522aad47b9
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-16 18:27:42 +0300

    deleted

--- src/Warnings/IncorrectValueInIsPositive.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-import * as texts from './texts.js';
-const IncorrectValueInIsPositive = () => {
-    console.log(texts.WarningMessage);
-    console.log(texts.IncorrectValueInIsPositive)
-}
-export default IncorrectValueInIsPositive;
