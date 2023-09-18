b0c4b8855872af3502892100391d82a2f374baab
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-18 17:25:39 +0300

    will be deleted soon

---
+++ src/Models/ForAllSetEdition.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * @external {validator}
+ * @link {../validator.js}
+ * A callback function for every of the
+ * elements of a set.
+ * @callback Callback
+ * @param {validator} element
+ * @param {number} index
+ * @returns {boolean}
+ **/
+/**
+ * @param {any} value
+ * @param {Callback} callback 
+ **/
+export const ForAllSetEdition = (value, callback) => {
+  let i, j, question = false;
+  return question;
+}
