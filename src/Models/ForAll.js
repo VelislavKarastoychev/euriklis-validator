d02eed1a83a361f79ab673c6450ab853ed5b3cce
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-22 21:55:32 +0300

    fixed callback and JSDoc.

--- src/Models/ForAll.js
+++ src/Models/ForAll.js
@@ -4,7 +4,7 @@ import validator from '../validator.js';
  * Implementation of the for_all method when the "value" property of the 
  * current validator instance is array.
  * @param {any} value - the "value" property of the current validator instance.
- * @param {function(validator, number)} callback
+ * @param {function(validator, number):validator} callback
  * @returns {boolean} if the callback function returns truthy for all the elements of the array, returns true, otherwise returns false.
  **/
 export const ForAllArrayEdition = (value, callback) => {
@@ -12,44 +12,44 @@ export const ForAllArrayEdition = (value, callback) => {
   let i, j, question = true;;
   for (i = 0;i < n >> 2;i++) {
     j = i << 2;
-    if (!callback(new validator(value[j]), j)) {
+    if (!callback(new validator(value[j]), j).answer) {
       question = false;
       break;
     }
     ++j;
-    if (!callback(new validator(value[j]), j)) {
+    if (!callback(new validator(value[j]), j).answer) {
       question = false;
       break;
     }
     ++j;
-    if (!callback(new validator(value[j]), j)) {
+    if (!callback(new validator(value[j]), j).answer) {
       question = false;
       break;
     }
     ++j;
-    if (!callback(new validator(value[j]), j)) {
+    if (!callback(new validator(value[j]), j).answer) {
       question = false;
       break;
     }
   }
   if (n % 4 >= 3 && question) {
     j = n - 3;
-    question = callback(new validator(value[j]), j); 
+    question = callback(new validator(value[j]), j).answer; 
   }
   if (n % 4 >= 2 && question) {
     j = n - 2;
-    question = callback(new validator(value[j]), j);
+    question = callback(new validator(value[j]), j).answer;
   }
   if (n % 4 >= 1 && question) {
     j = n - 1;
-    question = callback(new validator(value[j]), j);
+    question = callback(new validator(value[j]), j).answer;
   }
   return question;
 }
 /**
  * Implements the for_all method in the case, where the "value" property is object
  * @param {any} value - the value of the current validator instance.
- * @param {function(validator, number):boolean} callback
+ * @param {function(validator, number):validator} callback
  * @returns {boolean} if the callback function returns truthy values for all the elements of the array, then returns true, otherwise returns false.
  **/
 export const ForAllObjectEdition = (value, callback) => {
@@ -57,37 +57,37 @@ export const ForAllObjectEdition = (value, callback) => {
   let i, j, question = true;
   for (i = 0;i < n >> 2;i++) {
     j = i << 2;
-    if (!callback(new validator(value[keys[j]]), j)) {
+    if (!callback(new validator(value[keys[j]]), j).answer) {
       question = false;
       break;
     }
     ++j;
-    if (!callback(new validator(value[keys[j]]), j)) {
+    if (!callback(new validator(value[keys[j]]), j).answer) {
       question = false;
       break;
     }
     ++j;
-    if (!callback(new validator(value[keys[j]]), j)) {
+    if (!callback(new validator(value[keys[j]]), j).answer) {
       question = false;
       break;
     }
     ++j;
-    if (!callback(new validator(value[keys[j]]), j)) {
+    if (!callback(new validator(value[keys[j]]), j).answer) {
       question = false;
       break;
     }
   }
   if (n % 4 >= 3 && question) {
     j = n - 3;
-    question = callback(new validator(value[keys[j]]), j);
+    question = callback(new validator(value[keys[j]]), j).answer;
   }
   if (n % 4 >= 2 && question) {
     j = n - 2; 
-    question = callback(new validator(value[keys[j]]), j);
+    question = callback(new validator(value[keys[j]]), j).answer;
   }
   if (n % 4 >= 1 && question) {
     j = n - 1;
-    question = callback(new validator(value[keys[j]]), j);
+    question = callback(new validator(value[keys[j]]), j).answer;
   }
   return question;
 }
@@ -95,13 +95,13 @@ export const ForAllObjectEdition = (value, callback) => {
  * Utility function which is used in the implementation
  * of the for_all method.
  * @param {Set} value
- * @param {function(validator):boolean} callback
+ * @param {function(validator):validator} callback
  * @returns {boolean}
  **/
 export const ForAllSetEdition = (value, callback) => {
   let question = false;
   for (const element of value) {
-    if (!callback(new validator(element))) {
+    if (!callback(new validator(element)).answer) {
       question = false;
       break;
     }
@@ -112,13 +112,13 @@ export const ForAllSetEdition = (value, callback) => {
  * Utility function which is used in the implementation
  * of the for_all method.
  * @param {Map} map
- * @param {function(validator, number | string):boolean} callback
+ * @param {function(validator, number | string):validator} callback
  * @returns {boolean}
  **/
 export const ForAllMapEdition = (map, callback) => {
   let question = true; 
  for (const [key, value] of map) {
-    if (!callback(new validator(value), key)) {
+    if (!callback(new validator(value), key).answer) {
       question = false;
       break;
     }
