2fbefe1ef081a9a0092f583cbddea9ace948953f
VelislavKarastoychev <exel_mmm@abv.bg>
2023-09-28 09:20:54 +0300

    fixed error with this.value to value. More clear JSDoc description.

--- src/Models/IsIntegerLike.js
+++ src/Models/IsIntegerLike.js
@@ -1,11 +1,11 @@
 "use strict";
 /**
  * Tests if the value parameter is an integer or a string which can be transformed to integer.
- * Utility function for isIntegerLike method.
+ * Utility function for isConvertibleToInteger method.
  * @param {number | string} value
  * @returns {boolean}
  */
 export const IsIntegerLike = (value) =>
-  typeof this.value === "string"
+  typeof value === "string"
     ? Number.isInteger(+value)
     : Number.isInteger(value);
