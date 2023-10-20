3aa2ba5ff28e80153f2fc9b45ddb5f6e15a4a20d
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-21 00:04:10 +0300

    Implemented with more efficient way

--- src/Models/HasNaNInTypedArray.js
+++ src/Models/HasNaNInTypedArray.js
@@ -1,45 +1,36 @@
 "use strict";
 /**
- * This utility function tests if the underlined typed array contains a NaN value.
+ * Checks if in the elements of the typed array exists a NaN element.
+ * Utility funciton used in the isNumberArray, isIntegerArray etc.
+ * 
  * @param { Int8Array | Uint8ClampedArray |  Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array} value
- * @returns {false}
- **/
+ * @returns {boolean} if all the elements of the array are numbers
+ * returns false, otherwise returns true.
+ */
 export const HasNaNInTypedArray = (value) => {
   const n = value.length;
-  let i, j, question = false;
-  for (i = 0;i < n >> 2;i++) {
+  let i, j, hasNaN = false;
+  for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (isNaN(value[j])) {
-      question = true;
-      break;
-    }
-    ++j;
-    if (isNaN(value[j])) {
-      question = true;
-      break;
-    }
-    ++j;
-    if (isNaN(value[j])) {
-      question = true;
-      break;
-    }
-    ++j;
-    if (isNaN(value[j])) {
-      question = true;
+    if (
+      isNaN(value[j]) ||
+      isNaN(value[j + 1]) ||
+      isNaN(value[j + 2]) ||
+      isNaN(value[j + 3])
+    ) {
+      hasNaN = true;
       break;
     }
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    question = isNaN(value[j]);
+  if (!hasNaN) {
+    j = i << 2;
+    for (; j < n; j++) {
+      if (isNaN(value[j])) {
+        hasNaN = true;
+        break;
+      }
+    }
   }
-  if (n % 4 >= 2 && question) {
-    j = n - 2;
-    question = isNaN(value[j]);
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    question = isNaN(value[j]);
-  }
-  return question;
-}
+  
+  return hasNaN;
+};
