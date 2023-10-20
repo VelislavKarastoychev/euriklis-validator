818c0183aaea691d138b5ef7aaec21a7eab42de0
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-20 23:26:56 +0300

    Implementation is made with more efficient way , making multiple tests at once.

--- src/Models/IsNumberArray.js
+++ src/Models/IsNumberArray.js
@@ -1,50 +1,36 @@
 "use strict";
 /**
- * Implements the isNumberArray method.
+ * Checks if the value is an array of number elements
+ * utility funtion for the isNumberArray method.
  *
  * @param {number []} value - the current validator value.
- * @returns {boolean} if the "value" is a number array returns true, otherwise returns false.
- **/
+ * @returns {boolean} if the value is number array returns
+ * true, otherwise returns false.
+ */
 export const IsNumberArray = (value) => {
-  let i, j, question = true;
+  let i, j, isAllNumbers = true;
   const n = value.length;
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (typeof value[j] !== "number" && isNaN(value[j])) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "number" && isNaN(value[j])) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "number" && isNaN(value[j])) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "number" && isNaN(value[j])) {
-      question = false;
+    if (
+      (typeof value[j] !== "number" && isNaN(value[j])) ||
+      (typeof value[j + 1] !== "number" && isNaN(value[j + 1])) ||
+      (typeof value[j + 2] !== "number" && isNaN(value[j + 2])) ||
+      (typeof value[j + 3] !== "number" && isNaN(value[j + 3]))
+    ) {
+      isAllNumbers = false;
       break;
     }
   }
-  if (n % 4 === 3 && question) {
-    j = n - 3;
-    if (typeof value[j] !== "number" && isNaN(value[j])) {
-      question = false;
+  if (isAllNumbers) {
+    j = i << 2;
+    for (;j < n;j++) {
+      if (typeof value[j] !== "number" && isNaN(value[j])) {
+        isAllNumbers = false;
+        break;
+      }
     }
   }
-  if (n % 4 === 2 && question) {
-    j = n - 2;
-    if (typeof value[j] !== "number" && isNaN(value[j])) {
-      question = false;
-    }
-  }
-  if (n % 4 === 1 && question) {
-    j = n - 1;
-    question = false;
-  }
-  return question;
+  
+  return isAllNumbers;
 };
