4476119d385d9e1ceae2bd4bd4449183f6e3ff56
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-20 23:01:19 +0300

    implemented with more optimal loop technique, checking multiple values at once.

--- src/Models/IsBooleanArray.js
+++ src/Models/IsBooleanArray.js
@@ -2,52 +2,36 @@
 /**
  * Tests if an array contains only boolean elements.
  * Utility function for the isBooleanArray method.
+ *
  * @param {Array.<boolean>} value
  * @returns {boolean}
  */
 export const IsBooleanArray = (value) => {
   const n = value.length;
-  let i, j, question = true;
+  let i, j, isAllBoolean = true;
+
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (typeof value[j] !== "boolean") {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "boolean") {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "boolean") {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "boolean") {
-      question = false;
+    if (
+      typeof value[j] !== "boolean" ||
+      typeof value[j + 1] !== "boolean" ||
+      typeof value[j + 2] !== "boolean" ||
+      typeof value[j + 3] !== "boolean"
+    ) {
+      isAllBoolean = false;
       break;
     }
   }
-  if (question && (n % 4 >= 3)) {
-    j = n - 3;
-    if (typeof value[j] !== "boolean") {
-      question = false;
-    }
-  }
-  if (question && (n % 4 >= 2)) {
-    j = n - 2;
-    if (typeof value[j] !== "boolean") {
-      question = false;
-    }
-  }
-  if (question && (n % 4 >= 1)) {
-    j = n - 1;
-    if (typeof value[j] !== "boolean") {
-      question = false;
-    }
-  }
-  return question;
-};
 
+  if (isAllBoolean) {
+    j = i << 2;
+    for (;j < n;j++) {
+      if (typeof value[j] !== 'boolean') {
+        isAllBoolean = false;
+        break;
+      }
+    }
+  }
+  
+  return isAllBoolean;
+};
