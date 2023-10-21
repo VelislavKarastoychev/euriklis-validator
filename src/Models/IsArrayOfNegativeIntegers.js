da78cf7854766182e46954ed08997eedcccead0d
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-21 14:03:38 +0300

    Re implemented with more efficient way. JSDoc more clear

--- src/Models/IsArrayOfNegativeIntegers.js
+++ src/Models/IsArrayOfNegativeIntegers.js
@@ -1,84 +1,47 @@
 "use strict";
 /**
- * Implementation of the isArrayOfNegativeIntegers method.
+ * Checks if the value is an array of negative integers.
+ * Utility function for the isArrayOfNegativeIntegers method.
  *
- * @param {number []} value - the current validator property which has to be an array of negative integers.
- * @returns {boolean} if the "value" is an array of negative integers returns true, otherwise, returns false.
+ * @param {number []} value
+ * @returns {boolean} if the "value" is an array of negative
+ * integers returns true, otherwise, returns false.
  */
 export const IsArrayOfNegativeIntegers = (value) => {
-  let i, j, question = true;
   const n = value.length;
+  let i, j, areAllNegativeIntegers = true;
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
     if (
-      typeof value[j] !== "number" || !Number.isInteger(value[j])
+      (typeof value[j] !== "number" || !Number.isInteger(value[j]) ||
+        !(value[j] < 0))(
+          typeof value[j + 1] !== "number" || !Number.isInteger(value[j + 1]) ||
+            !(value[j + 1] < 0),
+        )(
+          typeof value[j + 2] !== "number" || !Number.isInteger(value[j + 2]) ||
+            !(value[j + 2] < 0),
+        )(
+          typeof value[j + 3] !== "number" || !Number.isInteger(value[j + 3]) ||
+            !(value[j + 3] < 0),
+        )
     ) {
-      question = false;
-      break;
-    } else if (value[j] >= 0) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (
-      typeof value[j] !== "number" || !Number.isInteger(value[j])
-    ) {
-      question = false;
-      break;
-    } else if (value[j] >= 0) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (
-      typeof value[j] !== "number" || !Number.isInteger(value[j])
-    ) {
-      question = false;
-      break;
-    } else if (value[j] >= 0) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (
-      typeof value[j] !== "number" || !Number.isInteger(value[j])
-    ) {
-      question = false;
-      break;
-    } else if (value[j] >= 0) {
-      question = false;
+      areAllNegativeIntegers = false;
       break;
     }
   }
-  if (question && (n % 4 >= 3)) {
-    j = n - 3;
-    if (
-      typeof value[j] !== "number" || !Number.isInteger(value[j])
-    ) {
-      question = false;
-    } else if (value[j] >= 0) {
-      question = false;
+
+  if (areAllNegativeIntegers) {
+    j = i << 2;
+    for (; j < n; j++) {
+      if (
+        (typeof value[j] !== "number" || !Number.isInteger(value[j]) ||
+          !(value[j] < 0))
+      ) {
+        areAllNegativeIntegers = false;
+        break;
+      }
     }
   }
-  if (question && (n % 4 >= 2)) {
-    j = n - 2;
-    if (
-      typeof value[j] !== "number" || !Number.isInteger(value[j])
-    ) {
-      question = false;
-    } else if (value[j] >= 0) {
-      question = false;
-    }
-  }
-  if (question && (n % 4 >= 1)) {
-    j = n - 1;
-    if (
-      typeof value[j] !== "number" || !Number.isInteger(value[j])
-    ) {
-      question = false;
-    } else if (value[j] >= 0) {
-      question = false;
-    }
-  }
-  return question;
+
+  return areAllNegativeIntegers;
 };
