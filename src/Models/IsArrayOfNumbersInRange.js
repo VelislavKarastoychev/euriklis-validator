fe1371e583d95f00d8c0d3dbf5b5ef4a8048271f
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-22 16:15:22 +0300

    Re-implemented with more efficient way and changed JSDoc

--- src/Models/IsArrayOfNumbersInRange.js
+++ src/Models/IsArrayOfNumbersInRange.js
@@ -1,7 +1,8 @@
 "use strict";
 /**
- * Implements the isArrayOfNumbersInRange method.
- *
+ * Checks if the "value" is an array of numbers in some open range (a, b).
+ * Utility function for the isArrayOfNumbersInRange method.
+ * 
  * @param {number []} value - the current validator instance value.
  * @param {number} a - the lower bound of the elements of the "value" array.
  * @param {number} b - the upper bound of the "value" elements in the array.
@@ -9,67 +10,37 @@
  */
 
 export const IsArrayOfNumbersInRange = (value, a, b) => {
-  let i, j, question = true;
   const n = value.length;
+  let i, j, areAllNumbersInRange = true;
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (typeof value[j] === "number" && !isNaN(value[j])) {
-      if (!(value[j] > a && value[b] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] === "number" && !isNaN(value[j])) {
-      if (!(value[j] > a && value[b] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] === "number" && !isNaN(value[j])) {
-      if (!(value[j] > a && value[b] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] === "number" && !isNaN(value[j])) {
-      if (!(value[j] > a && value[b] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
+    if (
+      (typeof value[j] !== "number" || isNaN(value[j]) || value[j] <= a ||
+        value[j] >= b) ||
+      (typeof value[j + 1] !== "number" || isNaN(value[j + 1]) ||
+        value[j + 1] <= a || value[j + 1] >= b) ||
+      (typeof value[j + 2] !== "number" || isNaN(value[j + 2]) ||
+        value[j + 2] <= a || value[j + 2] >= b) ||
+      (typeof value[j + 3] !== "number" || isNaN(value[j + 3]) ||
+        value[j + 3] <= a || value[j + 3] >= b)
+    ) {
+      areAllNumbersInRange = false;
       break;
     }
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    if (typeof value[j] === "number" && !isNaN(value[j])) {
-      question = value[j] > a && value[j] < b;
-    } else question = false;
+
+  if (areAllNumbersInRange) {
+    j = i << 2;
+    for (; j < n; j++) {
+      if (
+        (typeof value[j] !== "number" || isNaN(value[j]) || value[j] <= a ||
+          value[j] >= b)
+      ) {
+        areAllNumbersInRange = false;
+        break;
+      }
+    }
   }
-  if (n % 4 >= 2 && question) {
-    j = n - 2;
-    if (typeof value[j] === "number" && !isNaN(value[j])) {
-      question = value[j] > a && value[j] < b;
-    } else question = false;
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    if (typeof value[j] === "number" && !isNaN(value[j])) {
-      question = value[j] > a && value[j] < b;
-    } else question = false;
-  }
-  return question;
+
+  return areAllNumbersInRange;
 };
