bf841e643c8102c451c1804ce17a45f552bc71da
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-23 20:35:29 +0300

    reimplemented IsArrayOfIntegers, more clear JSDoc.

--- src/Models/IsArrayOfIntegersInRange.js
+++ src/Models/IsArrayOfIntegersInRange.js
@@ -1,74 +1,44 @@
 "use strict";
 /**
- * Implements the isArrayOfIntegersInRange method.
- *
+ * Checks if the "value" is an array of integers in the
+ * open interval (a, b).
+ * Utility function for the isArrayOfIntegersInRange.
+ * 
  * @param {number []} value an array of integers.
  * @param {number} a - the lower bound of the value elements.
  * @param {number} b - the upper bound of the value elements.
  * @returns {boolean} if the "value" property is integer array in the open interval (a, b) returns true, otherwise returns false.
- **/
+ */
 export const IsArrayOfIntegersInRange = (value, a, b) => {
   const n = value.length;
-  let i, j, question = true;
+  let i, j, areAllIntegersInRange = true;
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (Number.isInteger(value[j])) {
-      if (!(value[j] > a && value[j] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (Number.isInteger(value[j])) {
-      if (!(value[j] > a && value[j] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (Number.isInteger(value[j])) {
-      if (!(value[j] > a && value[j] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (Number.isInteger(value[j])) {
-      if (!(value[j] > a && value[j] < b)) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
+    if (
+      (!Number.isInteger(value[j]) || !(value[j] > a && value[j] < b)) ||
+      (!Number.isInteger(value[j + 1]) ||
+        !(value[j + 1] > a && value[j + 1] < b)) ||
+      (!Number.isInteger(value[j + 2]) ||
+        !(value[j + 2] > a && value[j + 2] < b)) ||
+      (!Number.isInteger(
+        value[j + 3],
+      ) || !(value[j + 3] > a && value[j + 3] < b))
+    ) {
+      areAllIntegersInRange = false;
       break;
     }
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    if (Number.isInteger(value[j])) {
-      question = value[j] > a && value[j] < b;
-    } else question = false;
+  if (areAllIntegersInRange) {
+    j = i << 2;
+    for (; j < n; j++) {
+      if (
+        (!Number.isInteger(value[j]) || !(value[j] > a && value[j] < b))
+      ) {
+        areAllIntegersInRange = false;
+        break;
+      }
+    }
   }
-  if (n % 4 >= 2 && question) {
-    j = n - 2;
-    if (Number.isInteger(value[j])) {
-      question = value[j] > a && value[j] < b;
-    } else question = false;
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    if (Number.isInteger(value[j])) {
-      question = value[j] > a && value[j] < b;
-    } else question = false;
-  }
-  return question;
+
+  return areAllIntegersInRange;
 };
