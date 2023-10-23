a7e2f9ad71f10e93c981af6bf3449bc42310bd67
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-23 21:13:49 +0300

    Reimplemented function for more efficient performance and more clear JSDoc.

--- src/Models/IsArrayOfIntegersInClosedRange.js
+++ src/Models/IsArrayOfIntegersInClosedRange.js
@@ -1,74 +1,43 @@
 "use strict";
 /**
- * Implements the is_array_of_integers_in_closed_range() method.
- *
+ * Checks if the "value" property is an array of integers, which lies
+ * in the closed interval [a, b].
+ * Utility function for the isArrayOfIntegersInClosedRange.
+ * 
  * @param {number []} value
  * @param {number} a - the lower bound of the array elements.
  * @param {number} b - the upper bound of the array elements.
  * @returns {boolean} if the "value" is integer array in the closed range [a, b], returns true, otherwise returns false.
  */
 export const IsArrayOfIntegersInClosedRange = (value, a, b) => {
-  let i, j, question = true;
   const n = value.length;
+  let i, j, areAllIntegersInClosedRange = true;
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (Number.isInteger(value[i])) {
-      if (value[j] < a || value[j] > b) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (Number.isInteger(value[i])) {
-      if (value[j] < a || value[j] > b) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (Number.isInteger(value[i])) {
-      if (value[j] < a || value[j] > b) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (Number.isInteger(value[i])) {
-      if (value[j] < a || value[j] > b) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
+    if (
+      (!Number.isInteger(value[j]) || value[j] < a || value[j] > b) ||
+      (!Number.isInteger(value[j + 1]) || value[j + 1] < a ||
+        value[j + 1] > b) ||
+      (!Number.isInteger(value[j + 2]) || value[j + 2] < a ||
+        value[j + 2] > b) ||
+      (!Number.isInteger(value[j + 3]) || value[j + 3] < a || value[j + 3] > b)
+    ) {
+      areAllIntegersInClosedRange = false;
       break;
     }
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    if (Number.isInteger(value[j])) {
-      question = !(value < a || value > b);
-    } else question = false;
+
+  if (areAllIntegersInClosedRange) {
+    j = i << 2;
+    for (; j < n; j++) {
+      if (
+        (!Number.isInteger(value[j]) || value[j] < a || value[j] > b)
+      ) {
+        areAllIntegersInClosedRange = false;
+        break;
+      }
+    }
   }
-  if (n % 4 >= 2 && question) {
-    j = n - 2;
-    if (Number.isInteger(value[j])) {
-      question = !(value < a || value > b);
-    } else question = false;
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    if (Number.isInteger(value[j])) {
-      question = !(value < a || value > b);
-    } else question = false;
-  }
-  return question;
+
+  return areAllIntegersInClosedRange;
 };
