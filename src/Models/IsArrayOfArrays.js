8a098000d2fb421b8efd0d1b1219e2f2db44ede0
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-24 23:29:55 +0300

    Implemented IsArrayOfArrays method.

---
+++ src/Models/IsArrayOfArrays.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import { CheckType } from "./CheckType";
+
+/**
+ * Checks if the "value" is an array of arbitrary array
+ * elements.
+ * 
+ * @param {[][]} value - an array of arbitrary arrays.
+ * @returns {boolean} if the "value" is an array whoose
+ * elements are all arbitrary arrays.
+ */
+export const IsArrayOfArray = (value) => {
+  const n = value.length;
+  let i, j;
+  for (i = 0;i < n >> 2;i++) {
+    j = i << 2;
+    if (
+      !CheckType(value[j], "Array") ||
+      !CheckType(value[j + 1], "Array") ||
+      !CheckType(value[j + 2], "Array") ||
+      !CheckType(value[j + 3], "Array") 
+    ) return false;
+  }
+
+  j = i << 2;
+  for (;j < n;j++) {
+    if (!CheckType(value[j], "Array")) return false;
+  }
+  return true;
+}
