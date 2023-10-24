546879817f86a41ba076d12cc09fd3308de4832b
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-25 00:51:59 +0300

    Re implemented (revised) IsArrayOfNumberArraysWIthEqualSize.JSDoc more clear.

--- src/Models/IsArrayOfNumberArraysWithEqualSize.js
+++ src/Models/IsArrayOfNumberArraysWithEqualSize.js
@@ -1,76 +1,32 @@
 "use strict";
-
 import { IsNumberArray } from "./IsNumberArray.js";
 
 /**
- * Implements IsArrayOfNumberArraysWithEqualSize method.
+ * Checks if the "value" is a numeric matrix.
  *
  * @param {number [][]} value - the current validator instance value.
- * @returns {boolean} If the "value" is a numeric matrix returns true, otherwise returns false.
+ * @returns {boolean} If the "value" is a numeric matrix
+ * returns true, otherwise returns false.
  */
 export const IsArrayOfNumberArraysWithEqualSize = (value) => {
-  let i, j, question = true;
   const n = value.length;
-  const l = value[0] instanceof Array ? value[0].length : -1;
+  let i, j, l;
+  if (!IsNumberArray(value[0])) return false;
+  else l = value[0].length;
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (value[j] instanceof Array) {
-      if (!IsNumberArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (value[j] instanceof Array) {
-      if (!IsNumberArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (value[j] instanceof Array) {
-      if (!IsNumberArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (value[j] instanceof Array) {
-      if (!IsNumberArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
+    if (
+      !IsNumberArray(value[j]) || value[j].length !== l ||
+      !IsNumberArray(value[j + 1]) || value[j + 1].length !== l ||
+      !IsNumberArray(value[j + 2]) || value[j + 2].length !== l ||
+      !IsNumberArray(value[j + 3]) || value[j + 3].length !== l
+    ) return false;
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-   if (value[j] instanceof Array) {
-      question = IsNumberArray(value[j]) && value[j].length === l;
-    } else question = false;
+
+  j = i << 2;
+  for (;j < n;j++) {
+    if (!IsNumberArray(value[j]) || value[j].length !== l) return false;
   }
-  if (n % 4 >= 2 && question) {
-    j = n - 2;
-    if (value[j] instanceof Array) {
-      question = IsNumberArray(value[j]) && value[j].length === l;
-    } else question = false;
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    if (value[j] instanceof Array) {
-      question = IsNumberArray(value[j]) && value[j].length === l;
-    } else question = false;
-  }
-  return question;
+
+  return true;
 };
