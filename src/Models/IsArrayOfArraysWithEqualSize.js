7133e134fe43b2d56ad0661ade28e834bc870deb
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-25 00:20:51 +0300

    Revised implementation of the IsArrayOfArraysWithEqualSize and more clear JSDoc.

--- src/Models/IsArrayOfArraysWithEqualSize.js
+++ src/Models/IsArrayOfArraysWithEqualSize.js
@@ -1,70 +1,38 @@
 "use strict";
+
+import { CheckType } from "./CheckType";
+
 /**
- * Implemants the isArrayOfArrraysWithEqualSize method.
+ * Checks if the "value" is an array of
+ * arbitrary arrays with equal size.
+ * Utility function for the isArrayOfArraysWithEqualSize method.
  *
- * @param {Array<Array>} value - an array of arrays which have to be with equal size.
- * @returns {boolean} if the "value" is an array of arrays with equal size returns true, otherwise returns false.
+ * @param {[][]} value - an array of arrays
+ * which have to be with equal size.
+ * @returns {boolean} Returns true if the value is an array
+ * which consists of arrays with equal size, otherwise
+ * returns false.
  */
 export const IsArrayOfArraysWithEqualSize = (value) => {
-  let i, j, question = true;
   const n = value.length;
-  const isFirstItemArray = value[0] instanceof Array;
-  const l1 = isFirstItemArray ? value[0].length : -1;
-  if (isFirstItemArray) {
-    for (i = 0; i < n >> 2; i++) {
-      j = i << 2;
-      if (value[j] instanceof Array) {
-        if (value[j].length !== l1) {
-          question = false;
-          break;
-        }
-      } else {
-        question = false;
-        break;
-      }
-      ++j;
-      if (value[j] instanceof Array) {
-        if (value[j].length !== l1) {
-          question = false;
-          break;
-        }
-      } else {
-        question = false;
-        break;
-      }
-      ++j;
-      if (value[j] instanceof Array) {
-        if (value[j].length !== l1) {
-          question = false;
-          break;
-        }
-      } else {
-        question = false;
-        break;
-      }
-      ++j;
-      if (value[j] instanceof Array) {
-        if (value[j].length !== l1) {
-          question = false;
-          break;
-        }
-      } else {
-        question = false;
-        break;
-      }
-    }
-    if (n % 4 >= 3 && question) {
-      j = n - 3;
-      question = value[j] instanceof Array ? value[j].length === l1 : false; 
-    }
-    if (n % 4 >= 2 && question) {
-      j = n - 2;
-      question = value[j] instanceof Array ? value[j].length === l1 : false;
-    }
-    if (n % 4 >= 1 && question) {
-      j = n - 1;
-      question = value[j] instanceof Array ? value[j].length === l1 : false;
-    }
-  } else question = false;
-  return question;
+  let i, j, l;
+  if (CheckType(value[0], "Array")) {
+    l = value[0].length;
+  } else return false;
+  for (i = 0; i < n >> 2; i++) {
+    j = i << 2;
+    if (
+      !CheckType(value[j], "Array") || value[j].length !== l ||
+      !CheckType(value[j + 1], "Array") || value[j].length !== l ||
+      !CheckType(value[j + 2], "Array") || value[j].length !== l ||
+      !CheckType(value[j + 3], "Array") || value[j].length !== l
+    ) return false;
+  }
+
+  j = i << 2;
+  for (; j < n; j++) {
+    if (!CheckType(value[j], "Array") || value[j].length !== l) return false;
+  }
+
+  return true;
 };
