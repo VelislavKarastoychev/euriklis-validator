405fe15f9ba24165fef9cb6f0f635552658bcbd9
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-24 22:10:05 +0300

    Reimplemented IsArrayOfFunctions and new JSDoc

--- src/Models/IsArrayOfFunctions.js
+++ src/Models/IsArrayOfFunctions.js
@@ -1,46 +1,30 @@
 "use strict";
 /**
- * Implements the isArrayOfFunctions method.
- * 
- * @param {Function []} value - the current validator instance, which has to be array of functions.
- * @returns {boolean} if the "value" is an array of functions, then returns true otherwise returns false.
- **/
+ * Checks if the "value" is an array of functions.
+ * Utility function for the isArrayOfFunctions method.
+ *
+ * @param {Function []} value - the current validator
+ * instance, which has to be array of functions.
+ * @returns {boolean} if the "value" is an array
+ * of functions, returns true, otherwise returns false.
+ */
 export const IsArrayOfFunctions = (value) => {
-  let i, j, question = true;
   const n = value.length;
-  for (i = 0;i < n >> 2;i++) {
+  let i, j;
+  for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (typeof value[j] !== "function") {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "function") {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "function") {
-      question = false;
-      break;
-    }
-    ++j;
-    if (typeof value[j] !== "function") {
-      question = false;
-      break;
-    }
+    if (
+      typeof value[j] !== "function" ||
+      typeof value[j + 1] !== "function" ||
+      typeof value[j + 2] !== "function" ||
+      typeof value[j + 3] !== "function"
+    ) return false;
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    question = typeof value[j] === "function";
+
+  j = i << 2;
+  for (; j < n; j++) {
+    if (typeof value[j] !== "function") return false;
   }
-  if (n % 4 >= 2 && question) {
-     j = n - 2;
-    question = typeof value[j] === "function";
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    question = typeof value[j] === "function";
-  }
-  return question;
-}
+
+  return true;
+};
