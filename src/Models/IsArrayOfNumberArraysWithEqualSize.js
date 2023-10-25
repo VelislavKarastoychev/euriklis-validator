c25a4e74dbc69f6f3249a6b4f10f0f80c976cbf7
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-25 15:10:50 +0300

    Fixed the IsArrayOfNumberArraysWithEqualSize wby using IsArray utilty function for more time efficient implementation.

--- src/Models/IsArrayOfNumberArraysWithEqualSize.js
+++ src/Models/IsArrayOfNumberArraysWithEqualSize.js
@@ -1,4 +1,5 @@
 "use strict";
+import { IsArray } from "./IsArray.js";
 import { IsNumberArray } from "./IsNumberArray.js";
 
 /**
@@ -11,7 +12,7 @@ import { IsNumberArray } from "./IsNumberArray.js";
 export const IsArrayOfNumberArraysWithEqualSize = (value) => {
   const n = value.length;
   let i, j, l;
-  if (!IsNumberArray(value[0])) return false;
+  if (!IsArray(value[0])) return false;
   else l = value[0].length;
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
