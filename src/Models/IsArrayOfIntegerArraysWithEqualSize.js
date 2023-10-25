4488cf8f8635d6c3dedbc9871e0eaf42b762f4f1
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-25 15:08:08 +0300

    Revised IsArrayOfIntegerArraysWithEqualSize and more clear JSDoc

--- src/Models/IsArrayOfIntegerArraysWithEqualSize.js
+++ src/Models/IsArrayOfIntegerArraysWithEqualSize.js
@@ -1,74 +1,32 @@
 "use strict";
+import { IsArray } from "./IsArray.js";
 import { IsIntegerArray } from "./IsIntegerArray.js";
 /**
  * Implements the isArrayOfIntegerArraysWithEqualSize.
- * 
+ *
  * @param {number [][]} value - the current validator value property.
  * @returns {boolean} if the "value" is an array of integer arrrays with equal size returns true, otherwise returns false.
- **/
+ */
 export const IsArrayOfIntegerArraysWithEqualSize = (value) => {
-  let question = true, i, j;
   const n = value.length;
-  const l = value[0] instanceof Array ? value[0].length : -1;
+  let i, j, l;
+  if (!IsArray(value[0])) return false;
+  else l = value[0].length;
+
   for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (value[j] instanceof Array) {
-      if (!IsIntegerArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (value[j] instanceof Array) {
-      if (!IsIntegerArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (value[j] instanceof Array) {
-      if (!IsIntegerArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
-    ++j;
-    if (value[j] instanceof Array) {
-      if (!IsIntegerArray(value[j]) || value[j].length !== l) {
-        question = false;
-        break;
-      }
-    } else {
-      question = false;
-      break;
-    }
+    if (
+      !IsIntegerArray(value[j]) || value[j].length !== l ||
+      !IsIntegerArray(value[j + 1]) || value[j + 1].length !== l ||
+      !IsIntegerArray(value[j + 2]) || value[j + 2].length !== l ||
+      !IsIntegerArray(value[j + 3]) || value[j + 3].length !== l
+    ) return false;
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    question = value[j] instanceof Array
-      ? IsIntegerArray(value[j]) && value[j].length === l
-      : false;
+  
+  j = i << 2;
+  for (;j < n;j++) {
+    if (!IsIntegerArray(value[j]) || value[j].length !== l) return false;
   }
-  if (n % 4 >= 2) {
-    j = n - 2;
-    question = value[j] instanceof Array
-      ? IsIntegerArray(value[j]) && value[j].length === l
-      : false;
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    question = value[j] instanceof Array
-      ? IsIntegerArray(value[j]) && value[j].length === l
-      : false;
-  }
-  return question;
+
+  return true;
 };
