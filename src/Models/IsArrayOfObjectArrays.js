775012e26655c58290aa7710b08b37a99f28ec11
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-26 18:18:44 +0300

    Fixed JSDoc for the value parameter.

--- src/Models/IsArrayOfObjectArrays.js
+++ src/Models/IsArrayOfObjectArrays.js
@@ -4,6 +4,8 @@ import { IsObjectArray } from "./IsObjectArray.js";
  * Checks if the "value" is an array of object arrays.
  * Utility function for the isArrayOfObjectArrays method.
  *
+ * @param {object [][]} value - the current validator parameter,
+ * which is expected to be an array of object arrays.
  * @returns {boolean} If all elements of the "value" are
  * object arrays, then returns true, otherwise returns false.
  */
