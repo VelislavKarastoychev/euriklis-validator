c39a8d6e94379db6a913ae5e9e0b5453a8e03795
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-26 17:57:26 +0300

    Implemented utility function IsArrayOfObjectArrays.

---
+++ src/Models/IsArrayOfObjectArrays.js
@@ -0,0 +1,29 @@
+"use strict";
+import { IsObjectArray } from "./IsObjectArray.js";
+/**
+ * Checks if the "value" is an array of object arrays.
+ * Utility function for the isArrayOfObjectArrays method.
+ *
+ * @returns {boolean} If all elements of the "value" are
+ * object arrays, then returns true, otherwise returns false.
+ */
+export const IsArrayOfObjectArrays = (value) => {
+  const n = value.length;
+  let i, j;
+  for (i = 0; i < n >> 2; i++) {
+    j = i << 2;
+    if (
+      !Array.isArray(value[j]) || !IsObjectArray(value[j]) ||
+      !Array.isArray(value[j + 1]) || !IsObjectArray(value[j + 1]) ||
+      !Array.isArray(value[j + 2]) || !IsObjectArray(value[j + 2]) ||
+      !Array.isArray(value[j + 3]) || !IsObjectArray(value[j + 3])
+    ) return false;
+  }
+
+  j = i << 2;
+  for (; j < n; j++) {
+    if (!Array.isArray(value[j]) || !IsObjectArray(value[j])) return false;
+  }
+
+  return true;
+};
