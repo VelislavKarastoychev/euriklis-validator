9c96f0808a735f771dd43cab01272a3b6ca8113d
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-27 22:42:41 +0300

    Renamed, reimplemented and new JSDoc.

--- src/Models/ForAll.js
+++ src/Models/ForAll.js
@@ -1,127 +1,115 @@
 "use strict";
-import validator from '../validator.js';
+import validator from "../validator.js";
 /**
- * Implementation of the for_all method when the "value" property of the 
- * current validator instance is array.
- * @param {any} value - the "value" property of the current validator instance.
- * @param {function(validator, number):validator} callback
- * @returns {boolean} if the callback function returns truthy for all the elements of the array, returns true, otherwise returns false.
- **/
-export const ForAllArrayEdition = (value, callback) => {
+ * Implementation of the forEvery method when the "value" property of the
+ * current validator instance is an array.
+ *
+ * @param {any} value - The "value" property of the current validator instance.
+ * @param {function(validator, number): validator} callback - A function that
+ * takes a validator instance and an optional key or index and returns a validator instance.
+ *
+ * @returns {boolean} True if the callback function returns "truthy" validator instances
+ * for all elements of the array, false otherwise.
+ */
+export const ForEveryArrayEdition = (value, callback) => {
   const n = value.length;
-  let i, j, question = true;;
-  for (i = 0;i < n >> 2;i++) {
+  const v = new validator();
+  let i, j;
+
+  for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (!callback(new validator(value[j]), j).answer) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (!callback(new validator(value[j]), j).answer) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (!callback(new validator(value[j]), j).answer) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (!callback(new validator(value[j]), j).answer) {
-      question = false;
-      break;
-    }
+    if (
+      (v.reset(), v.value = value[j], !callback(v, j).answer) ||
+      (v.reset(), v.value = value[j + 1], !callback(v, j + 1).answer) ||
+      (v.reset(), v.value = value[j + 2], !callback(v, j + 2).answer) ||
+      (v.reset(), v.value = value[j + 3], !callback(v, j + 3).answer)
+    ) return false;
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    question = callback(new validator(value[j]), j).answer; 
+
+  j = i << 2;
+  for (; j < n; j++) {
+    if ((v.reset(), v.value = value[j], !callback(v, j).answer)) return false;
   }
-  if (n % 4 >= 2 && question) {
-    j = n - 2;
-    question = callback(new validator(value[j]), j).answer;
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    question = callback(new validator(value[j]), j).answer;
-  }
-  return question;
-}
+
+  return true;
+};
+
 /**
- * Implements the for_all method in the case, where the "value" property is object
- * @param {any} value - the value of the current validator instance.
- * @param {function(validator, number):validator} callback
- * @returns {boolean} if the callback function returns truthy values for all the elements of the array, then returns true, otherwise returns false.
- **/
-export const ForAllObjectEdition = (value, callback) => {
+ * Implements the forEvery method for objects.
+ *
+ * @param {any} value - The value of the current validator instance.
+ * @param {function(validator, number): validator} callback - A function that
+ * takes a validator instance and an optional key or index and returns a validator instance.
+ *
+ * @returns {boolean} True if the callback function returns "truthy" validator instances
+ * for all values of the object, false otherwise.
+ */
+export const ForEveryObjectEdition = (value, callback) => {
   const keys = Object.keys(value), n = keys.length;
-  let i, j, question = true;
-  for (i = 0;i < n >> 2;i++) {
+  const v = new validator();
+  let i, j;
+
+  for (i = 0; i < n >> 2; i++) {
     j = i << 2;
-    if (!callback(new validator(value[keys[j]]), j).answer) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (!callback(new validator(value[keys[j]]), j).answer) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (!callback(new validator(value[keys[j]]), j).answer) {
-      question = false;
-      break;
-    }
-    ++j;
-    if (!callback(new validator(value[keys[j]]), j).answer) {
-      question = false;
-      break;
+    if (
+      (v.reset(), v.value = value[keys[j]], !callback(v, keys[j])) ||
+      (v.reset(), v.value = value[keys[j + 1]], !callback(v, keys[j + 1])) ||
+      (v.reset(), v.value = value[keys[j + 2]], !callback(v, keys[j + 2])) ||
+      (v.reset(), v.value = value[keys[j + 3]], !callback(v, keys[j + 3]))
+    ) return false;
+  }
+
+  j = i << 2;
+  for (; j < n; j++) {
+    if ((v.reset(), v.value = value[keys[j]], !callback(v, keys[j]))) {
+      return false;
     }
   }
-  if (n % 4 >= 3 && question) {
-    j = n - 3;
-    question = callback(new validator(value[keys[j]]), j).answer;
-  }
-  if (n % 4 >= 2 && question) {
-    j = n - 2; 
-    question = callback(new validator(value[keys[j]]), j).answer;
-  }
-  if (n % 4 >= 1 && question) {
-    j = n - 1;
-    question = callback(new validator(value[keys[j]]), j).answer;
-  }
-  return question;
-}
+
+  return true;
+};
+
 /**
- * Utility function which is used in the implementation
- * of the for_all method.
- * @param {Set} value
- * @param {function(validator):validator} callback
- * @returns {boolean}
- **/
-export const ForAllSetEdition = (value, callback) => {
-  let question = false;
+ * Implementation of the forEvery method when the "value" property of the
+ * current validator instance is a set.
+ *
+ * @param {any} value - The "value" property of the current validator instance.
+ * @param {function(validator, number): validator} callback - A function that
+ * takes a validator instance and an optional key or index and returns a validator instance.
+ *
+ * @returns {boolean} True if the callback function returns "truthy" validator instances
+ * for all elements of the set, false otherwise.
+ */
+export const ForEverySetEdition = (value, callback) => {
+  const v = new validator();
+  let index = 0;
+  
   for (const element of value) {
-    if (!callback(new validator(element)).answer) {
-      question = false;
-      break;
+    if (v.reset(), v.value = element, !callback(v, index++).answer) {
+      return false;
     }
   }
-  return question;
-}
+
+  return true;
+};
+
 /**
- * Utility function which is used in the implementation
- * of the for_all method.
- * @param {Map} map
- * @param {function(validator, number | string):validator} callback
- * @returns {boolean}
- **/
-export const ForAllMapEdition = (map, callback) => {
-  let question = true; 
- for (const [key, value] of map) {
-    if (!callback(new validator(value), key).answer) {
-      question = false;
-      break;
-    }
+ * Implementation of the forEvery method when the "value" property of the
+ * current validator instance is a map.
+ *
+ * @param {any} value - The "value" property of the current validator instance.
+ * @param {function(validator, number): validator} callback - A function that
+ * takes a validator instance and an optional key or index and returns a validator instance.
+ *
+ * @returns {boolean} True if the callback function returns "truthy" validator instances
+ * for all values of the map, false otherwise.
+ */
+export const ForEveryMapEdition = (map, callback) => {
+  const v = new validator();
+
+  for (const [key, value] of map) {
+    if (v.reset(), v.value = value, !callback(v, key).answer) return false;
   }
-  return question;
-}
+
+  return true;
+};
