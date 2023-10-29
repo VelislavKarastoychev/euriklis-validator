aa7e2d26c4a05184577d6de8e4222133cbc86136
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-29 11:08:13 +0200

    Fixed error.

--- src/Models/IsArrayOfNegativeIntegers.js
+++ src/Models/IsArrayOfNegativeIntegers.js
@@ -14,16 +14,17 @@ export const IsArrayOfNegativeIntegers = (value) => {
     j = i << 2;
     if (
       (typeof value[j] !== "number" || !Number.isInteger(value[j]) ||
-        !(value[j] < 0))(
-          typeof value[j + 1] !== "number" || !Number.isInteger(value[j + 1]) ||
-            !(value[j + 1] < 0),
-        )(
-          typeof value[j + 2] !== "number" || !Number.isInteger(value[j + 2]) ||
-            !(value[j + 2] < 0),
-        )(
-          typeof value[j + 3] !== "number" || !Number.isInteger(value[j + 3]) ||
-            !(value[j + 3] < 0),
-        )
+        !(value[j] < 0)) ||
+      (
+        typeof value[j + 1] !== "number" || !Number.isInteger(value[j + 1]) ||
+        !(value[j + 1] < 0)
+      ) || (
+        typeof value[j + 2] !== "number" || !Number.isInteger(value[j + 2]) ||
+        !(value[j + 2] < 0)
+      ) || (
+        typeof value[j + 3] !== "number" || !Number.isInteger(value[j + 3]) ||
+        !(value[j + 3] < 0)
+      )
     ) {
       return false;
     }
