973a1e60b5af2637c1d16787aa55fd2a2e1d5e3f
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-29 11:07:12 +0200

    Adapt to the new syntax

--- Tests/is-boolean-array.spec.js
+++ Tests/is-boolean-array.spec.js
@@ -1,6 +1,6 @@
 import validator from '../index.js';
 import { print, booleanArray } from './data.js';
-new validator(booleanArray).isBooleanArray.Or.isBoolean.on(true, (v) => {
+new validator(booleanArray).isBooleanArray.or.isBoolean.on(true, (v) => {
   const t1 = v.benchmark((b) => new validator(b).isBooleanArray.answer);
   const t2 = v.benchmark((
     b,
