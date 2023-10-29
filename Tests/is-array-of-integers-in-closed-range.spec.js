7407bd6bacb6bf7a46bc9d07c87e760b00390136
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-29 11:06:35 +0200

    Adapt to the new syntax

--- Tests/is-array-of-integers-in-closed-range.spec.js
+++ Tests/is-array-of-integers-in-closed-range.spec.js
@@ -1,11 +1,12 @@
 "use strict";
 import validator from "../index.js";
 import { intArray, print } from "./data.js";
-new validator(intArray).is_array_of_integers_in_closed_range(0, 100).on(
+
+new validator(intArray).isArrayOfIntegersInClosedRange(0, 100).on(
   true,
   (v) => {
     const t1 = v.benchmark((n) =>
-      new validator(n).is_array_of_integers_in_closed_range(0, 100).answer
+      new validator(n).isArrayOfIntegersInClosedRange(0, 100).answer
     );
     const t2 = v.benchmark((n) =>
       n instanceof Array
@@ -15,7 +16,7 @@ new validator(intArray).is_array_of_integers_in_closed_range(0, 100).on(
         : false
     );
     print(
-      "is_array_of_integers_in_closed_range(0, 100) with 1000000 elements",
+      "is array of integers in closed range (0, 100) with 1000000 elements",
       t1,
       t2,
     );
