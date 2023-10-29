18a5b39593eaae787e736de72bf7cd3f61cb3078
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-29 11:09:13 +0200

    Adapt to the new syntax

---
+++ Tests/for-every.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+import validator from "../index.js";
+import { print, matrix } from "./data.js";
+new validator(matrix).forEvery((item) => item.isArrayOfNumbersInRange(0, 1)).on(true, (v) => {
+  const t1 = v.benchmark((n) =>
+    new validator(n).forEvery((el) => el.isNumberArray).answer
+  );
+  const t2 = v.benchmark(
+    (n) =>
+      n instanceof Array
+        ? n.every((row) =>
+          row instanceof Array
+            ? row.every((el) => typeof el === "number" && !isNaN(el))
+            : false
+        )
+        : false,
+    1000,
+  );
+  print("for every", t1, t2);
+}).on(false, () => console.log("Error in for every"));
+
