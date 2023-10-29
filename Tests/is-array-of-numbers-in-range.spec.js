68b3848ff3472cba1e5c4c242b669ce6f6e16033
VelislavKarastoychev <exel_mmm@abv.bg>
2023-10-29 11:07:02 +0200

    Adapt to the new syntax

--- Tests/is-array-of-numbers-in-range.spec.js
+++ Tests/is-array-of-numbers-in-range.spec.js
@@ -1,11 +1,11 @@
 "use strict";
 import { numericArray, print } from "./data.js";
 import validator from "../index.js";
-new validator(numericArray).is_array_of_numbers_in_range(-1, 100).on(
+new validator(numericArray).isArrayOfNumbersInRange(-1, 100).on(
   true,
   (v) => {
     const t1 = v.benchmark((n) =>
-      new validator(n).is_array_of_numbers_in_range(-1, 100).answer
+      new validator(n).isArrayOfNumbersInRange(-1, 100).answer
     );
     const t2 = v.benchmark((n) =>
       n instanceof Array
@@ -17,7 +17,7 @@ new validator(numericArray).is_array_of_numbers_in_range(-1, 100).on(
         : false
     );
     print(
-      "is_array_of_numbers_in_range(-1, 100) with 1000000 elements",
+      "is_array of numbers in range (-1, 100) with 1000000 elements",
       t1,
       t2,
     );
