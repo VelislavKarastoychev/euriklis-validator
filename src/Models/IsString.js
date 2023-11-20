9090ed6e596d70877b36e205fc7e938f386e942d
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-20 22:21:17 +0200

    Deleted

--- src/Models/IsString.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Tests if the value property is a string type in javascript.
- * Utility function for isString method.
- * @param {any} value 
- * @returns {boolean}
- **/
-export const IsString = (value) => typeof value === 'string'
