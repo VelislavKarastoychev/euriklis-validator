b9f680d02d1a40d1f0fb571237db268c501e3e40
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-21 07:09:00 +0200

    Deleted because is not used anywhere

--- src/Models/IsAsyncFunction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Utility function which is used in the implementation of the isAsync method.
- * @param {AsyncGenerator} value - the current validator "value" property.
- * @returns {boolean} if the "value" is AsyncFunction, then returns true, otherwise returns false.
- */
-export const IsAsyncFunction = (value) =>
-  Object.prototype.toString.call(value) === "[object AsyncFunction]";
