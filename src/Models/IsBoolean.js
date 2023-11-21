7e222e6dc7b7dae6f4a149f0bb372b32c6b80772
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-21 06:50:07 +0200

    Deleted because is not used.

--- src/Models/IsBoolean.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Tests if a value is boolean type in javascript.
- * Utility function for the isBoolean method.
- * @param {any} value
- * @returns {boolean}
- **/
-export const IsBoolean = (value) => typeof value === 'boolean'
