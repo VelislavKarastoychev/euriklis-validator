0ae04a3dd71886e660233b835c88ea715272a830
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-21 12:49:03 +0200

    Deleted because is no using anywhere

--- src/Models/IsUndefined.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Tests if a value is undefined.
- * Utility function for the is_undefined() or isUndefined method.
- * @param {any} value
- * @returns {boolean}
- **/
-export const IsUndefined = (value) => typeof value === 'undefined'; 
