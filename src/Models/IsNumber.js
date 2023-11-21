47b582c596618ef687f18ab305f3b7631aeb1e3c
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-21 12:56:47 +0200

    Deleted

--- src/Models/IsNumber.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Tests if the value parameter is a number.
- * Utility function for the isNumber method.
- * @param {any} value
- * @returns {boolean}
- **/
-export const IsNumber = (value) => (typeof value === 'number') && !isNaN(value)
