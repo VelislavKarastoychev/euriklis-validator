7eec52b1a6094c4dc07e4655ac648dedbdf2462f
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-21 06:45:36 +0200

    Deleted because is not used anywhere

--- src/Models/IsArray.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Tests if the value parameter is Array type.
- * Utility function for the isArray method.
- * @param {Array} value
- * @returns {boolean}
- */
-export const IsArray = (value) => value instanceof Array;
