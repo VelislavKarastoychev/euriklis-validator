42652f914c8d170655b83a4365ed3cc07af8799b
VelislavKarastoychev <exel_mmm@abv.bg>
2023-11-21 07:13:47 +0200

    Deleted because is not used yet

--- src/Models/IsFloat32Array.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Utility function which is used in the implementation of the isFloat32Array method.
- * @param {Float32Array} value - the current validator "value" property
- * @returns {boolean} if the "value" is Float32Array returns true, otherwise returns false.
- **/
-export const IsFloat32Array = (value) => value instanceof Float32Array;
